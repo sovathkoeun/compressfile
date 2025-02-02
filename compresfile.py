fc7ba2216c9b9c3663af9ee2b60444e1cba74e9e
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-02 20:50:36 +0700

    add new code

--- compresfile.py
+++ compresfile.py
@@ -2,5 +2,8 @@ import zlib, base64
 
 data = open('demo.txt', 'r').read()
 data_byte = bytes(data, 'utf-8')
-compressed_data = base64.b16decode(zlib.compress(data_byte)) 
-print(compressed_data)
+compressed_data = base64.b64encode(zlib.compress(data_byte, 9))
+decoded_data = compressed_data.decode('utf-8')
+compressed_file = open('compressed.txt', 'w')
+compressed_file.write(decoded_data)
+
