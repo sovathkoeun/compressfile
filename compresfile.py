a6712e963e1781862af14b0191fed46fe38760fd
sovathkoeun <sovathkoeun2020@gmail.com>
2025-02-02 20:56:37 +0700

    add decompress

--- compresfile.py
+++ compresfile.py
@@ -7,3 +7,6 @@ decoded_data = compressed_data.decode('utf-8')
 compressed_file = open('compressed.txt', 'w')
 compressed_file.write(decoded_data)
 
+decompressed_data = zlib.decompress(base64.b64decode(compressed_data))
+print(decompressed_data.decode('utf-8'))
+
